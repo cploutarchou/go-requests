98dc2fbc6955a2c762117a8bebfc31e3e9024cb4
Christos Ploutarchou <cploutarchou@gmail.com>
2022-10-18 21:12:49 +0300

    Define custom and common headers

--- http/core.go
+++ http/core.go
@@ -19,7 +19,26 @@ func (c *client) do(method Method, url string, headers http.Header, body []byte)
 	if err != nil {
 		return nil, errors.New("unable to create request")
 	}
-	response, err := _client.Do(req)
-	return response, err
 
+	// Set all setted headers to the http request
+	availableHeaders := c.getHeaders(headers)
+	req.Header = availableHeaders
+	return _client.Do(req)
+}
+
+func (c *client) getHeaders(headers http.Header) http.Header {
+	res := make(http.Header)
+	// Set common headers to the request
+	for header, value := range c.Headers {
+		if len(value) > 0 {
+			res.Set(header, value[0])
+		}
+	}
+	// Set headers to the request
+	for header, value := range headers {
+		if len(value) > 0 {
+			res.Set(header, value[0])
+		}
+	}
+	return res
 }
