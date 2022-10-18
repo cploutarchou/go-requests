7fde60b5b77dc271265a40a872531a09459864f0
Christos Ploutarchou <cploutarchou@gmail.com>
2022-10-18 21:13:08 +0300

    Update example

--- example.go
+++ example.go
@@ -2,16 +2,35 @@ package main
 
 import (
 	"fmt"
-	"github.com/cploutarchou/go-http/http"
 	"io"
+
+	"github.com/cploutarchou/go-http/http"
 )
 
+var (
+	client http.Client
+)
+
+func getGithubClient() http.Client {
+	_client := http.NewClient()
+	commonHeaders := _client.MakeHeaders()
+	commonHeaders.Add("Accept", "application/json")
+	_client.SetHeaders(commonHeaders)
+	return _client
+}
+func init() {
+	client = getGithubClient()
+}
 func main() {
-	client := http.NewClient()
 	response, err := client.Get("https://api.github.com", nil)
+	client.MakeHeaders()
 	if err != nil {
 		panic(err)
 	}
+	defer response.Body.Close()
 	bytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println(string(bytes))
 }
