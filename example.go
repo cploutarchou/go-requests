7f1d3a60d00aa2e914b5017e83f6cf30483115ca
Christos Ploutarchou <cploutarchou@gmail.com>
2022-11-12 19:36:32 +0200

    Update examples

--- example.go
+++ example.go
@@ -8,36 +8,25 @@ import (
 	"github.com/cploutarchou/go-http/http"
 )
 
+var client http.Client
+
 func getGithubClientWithOutConfig() http.Client {
 	builder := http.NewBuilder()
 	builder.SetRequestTimeout(50 * time.Second).SetResponseTimeout(50 * time.Second).SetMaxIdleConnections(10)
-	http.Headers().SetHeaders(map[string][]string{
-		"Content-Type": {"application/json"},
-	}
-	builder.SetHeaders().
 	return builder.Build()
-}
 
-func getGithubClientWithConfig() http.Client {
-	_client := http.NewBuilder()
-	_client.SetConfig(&http.TimeoutSettings{
-		MaxIdleConnections: 10,
-		ResponseTimeout:    50 * time.Second,
-		RequestTimeout:     50 * time.Second,
-	})
-	return _client
 }
 
 func getGithubClientBySetters() http.Client {
 	_client := http.NewBuilder()
-	_client.SetRequestTimeout(50 * time.Second)
-	_client.SetResponseTimeout(50 * time.Second)
-	_client.SetMaxIdleConnections(10)
-	return _client
+	_client.SetRequestTimeout(50 * time.Second).SetResponseTimeout(50 * time.Second).SetMaxIdleConnections(10)
+	_client.Headers().SetAcceptCharset("utf-8").SetAccept("application/json")
+	return _client.Build()
+
 }
+
 func init() {
-	client = getGithubClientWithOutConfig()
-	client = getGithubClientWithConfig()
+	//client = getGithubClientWithOutConfig()
 	client = getGithubClientBySetters()
 }
 
@@ -61,12 +50,15 @@ func main() {
 
 func GetExample() {
 	response, err := client.Get("https://api.github.com", nil)
-	client.DisableTimeouts(true)
-	client.MakeHeaders()
 	if err != nil {
 		panic(err)
 	}
-	defer response.Body.Close()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(response.Body)
 	bytes, err := io.ReadAll(response.Body)
 	if err != nil {
 		panic(err)
@@ -76,11 +68,15 @@ func GetExample() {
 
 func PostExample(u User) {
 	response, err := client.Post("https://api.github.com", nil, u)
-	client.MakeHeaders()
 	if err != nil {
 		panic(err)
 	}
-	defer response.Body.Close()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(response.Body)
 	//bytes, err := io.ReadAll(response.Body)
 	//if err != nil {
 	//	panic(err)
