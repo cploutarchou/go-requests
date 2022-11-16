b345f847f8ae398dfbb021ba5033bd50eefb82f7
Christos Ploutarchou <cploutarchou@gmail.com>
2022-11-16 21:16:22 +0200

    Create test for post method

--- http/client_test.go
+++ http/client_test.go
@@ -60,3 +60,60 @@ func Test_goHTTPClient_Get(t *testing.T) {
 		})
 	}
 }
+
+func Test_goHTTPClient_Post(t *testing.T) {
+	type fields struct {
+		Headers Headers
+		Timeout Timeout
+	}
+	type args struct {
+		url     string
+		headers http.Header
+		body    interface{}
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *http.Response
+		wantErr bool
+	}{
+		{
+			name: "Test goHTTPClient Post",
+			fields: fields{
+				Headers: NewHeaders(),
+				Timeout: newTimeouts(),
+			},
+			args: args{
+				url:     "https://api.github.com",
+				headers: nil,
+				body:    nil,
+			},
+			want: &http.Response{
+				StatusCode: 200,
+				Status:     "200 OK",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				builder := NewBuilder()
+				client := builder.Build()
+				res, err := client.Post(tt.args.url, tt.fields.Headers.GetAll(), tt.args.body)
+				got := &http.Response{
+					StatusCode: res.StatusCode,
+					Status:     res.Status,
+				}
+				if (err != nil) != tt.wantErr {
+					t.Errorf("goHTTPClient.Post() error = %v, wantErr %v", err, tt.wantErr)
+					return
+				}
+				if !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("goHTTPClient.Post() = %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
