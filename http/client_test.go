437180f415203061027b073a0bb379902a419bc3
Christos Ploutarchou <cploutarchou@gmail.com>
2022-11-16 21:05:32 +0200

    Create test for get method

---
+++ http/client_test.go
@@ -0,0 +1,62 @@
+package http
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func Test_goHTTPClient_Get(t *testing.T) {
+	type fields struct {
+		Headers Headers
+		Timeout Timeout
+	}
+	type args struct {
+		url     string
+		headers http.Header
+	}
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *http.Response
+		wantErr bool
+	}{
+
+		{
+			name: "Test goHTTPClient Get",
+			fields: fields{
+				Headers: NewHeaders(),
+				Timeout: newTimeouts(),
+			},
+			args: args{
+				url:     "https://api.github.com",
+				headers: nil,
+			},
+			want: &http.Response{
+				StatusCode: 200,
+				Status:     "200 OK",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			builder := NewBuilder()
+			client := builder.Build()
+			res, err := client.Get(tt.args.url, tt.fields.Headers.GetAll())
+			got := &http.Response{
+				StatusCode: res.StatusCode,
+				Status:     res.Status,
+			}
+			if (err != nil) != tt.wantErr {
+				t.Errorf("goHTTPClient.Get() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("goHTTPClient.Get() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
