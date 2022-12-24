7bee60cff3fd5dd2c87afbfd1385ff10e0b3cd0a
Christos Ploutarchou <cploutarchou@gmail.com>
2022-12-25 00:17:40 +0200

    Create TestGetContasts test

--- examples/get_test.go
+++ examples/get_test.go
@@ -1,55 +1,61 @@
 package examples
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestGetEndpoint(t *testing.T) {
+func TestGetContasts(t *testing.T) {
 	type args struct {
 		endpoint string
 	}
 	tests := []struct {
 		name string
 		args args
-		want Endpoints
+		want []Contact
 	}{
 		{
 			name: "Test Get Endpoint",
 			args: args{
-				endpoint: "https://api.github.com",
+				endpoint: baseURL + "v1/contacts",
 			},
-			want: Endpoints{
-				CurrentUserURL:                   "https://api.github.com/user",
-				AuthorizationsURL:                "https://api.github.com/authorizations",
-				CurrentUserAuthorizationsHTMLURL: "https://github.com/settings/connections/applications{/client_id}",
-				RepositoryURL:                    "https://api.github.com/repos/{owner}/{repo}",
-				CodeSearchURL:                    "https://api.github.com/search/code?q={query}{&page,per_page,sort,order}",
-				CommitSearchURL:                  "https://api.github.com/search/commits?q={query}{&page,per_page,sort,order}",
+			want: []Contact{
+				{
+					ID:        "11111",
+					FirstName: "Tom",
+					LastName:  "Smith",
+					Email:     "tom.smith@example.com",
+					DateAdded: "2021-01-03",
+					CompanyID: "123",
+				},
+				{
+					ID:        "22222",
+					FirstName: "Suki",
+					LastName:  "Patel",
+					Email:     "spatel@example.com",
+					DateAdded: "2020-11-12",
+					CompanyID: "123",
+				},
+				{
+					ID:        "33333",
+					FirstName: "Lexine",
+					LastName:  "Barnfield",
+					Email:     "barnfield8@example.com",
+					DateAdded: "2021-01-03",
+					CompanyID: "234",
+				},
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := GetGithubEndpoints(tt.args.endpoint)
+			got, err := GetContacts(tt.args.endpoint)
 			if err != nil {
-				t.Errorf("GetGithubEndpoints() error = %v", err)
+				t.Errorf("GetContacts() error = %v", err)
 				return
 			}
-			if got.CurrentUserURL != tt.want.CurrentUserURL {
-				t.Errorf("GetGithubEndpoints() got = %v, want %v", got, tt.want)
-			}
-			if got.AuthorizationsURL != tt.want.AuthorizationsURL {
-				t.Errorf("GetGithubEndpoints() got = %v, want %v", got, tt.want)
-			}
-			if got.CurrentUserAuthorizationsHTMLURL != tt.want.CurrentUserAuthorizationsHTMLURL {
-				t.Errorf("GetGithubEndpoints() got = %v, want %v", got, tt.want)
-			}
-			if got.RepositoryURL != tt.want.RepositoryURL {
-				t.Errorf("GetGithubEndpoints() got = %v, want %v", got, tt.want)
-			}
-			if got.CodeSearchURL != tt.want.CodeSearchURL {
-				t.Errorf("GetGithubEndpoints() got = %v, want %v", got, tt.want)
-			}
-			if got.CommitSearchURL != tt.want.CommitSearchURL {
-				t.Errorf("GetGithubEndpoints() got = %v, want %v", got, tt.want)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetContacts() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
