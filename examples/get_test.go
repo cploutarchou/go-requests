e7a103e75c337f40add098ee09826c74394e7e95
Christos Ploutarchou <cploutarchou@gmail.com>
2022-12-24 10:43:08 +0200

    Create test for get example

---
+++ examples/get_test.go
@@ -0,0 +1,56 @@
+package examples
+
+import "testing"
+
+func TestGetEndpoint(t *testing.T) {
+	type args struct {
+		endpoint string
+	}
+	tests := []struct {
+		name string
+		args args
+		want Endpoints
+	}{
+		{
+			name: "Test Get Endpoint",
+			args: args{
+				endpoint: "https://api.github.com",
+			},
+			want: Endpoints{
+				CurrentUserURL:                   "https://api.github.com/user",
+				AuthorizationsURL:                "https://api.github.com/authorizations",
+				CurrentUserAuthorizationsHTMLURL: "https://github.com/settings/connections/applications{/client_id}",
+				RepositoryURL:                    "https://api.github.com/repos/{owner}/{repo}",
+				CodeSearchURL:                    "https://api.github.com/search/code?q={query}{&page,per_page,sort,order}",
+				CommitSearchURL:                  "https://api.github.com/search/commits?q={query}{&page,per_page,sort,order}",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetEndpoint(tt.args.endpoint)
+			if err != nil {
+				t.Errorf("GetEndpoint() error = %v", err)
+				return
+			}
+			if got.CurrentUserURL != tt.want.CurrentUserURL {
+				t.Errorf("GetEndpoint() got = %v, want %v", got, tt.want)
+			}
+			if got.AuthorizationsURL != tt.want.AuthorizationsURL {
+				t.Errorf("GetEndpoint() got = %v, want %v", got, tt.want)
+			}
+			if got.CurrentUserAuthorizationsHTMLURL != tt.want.CurrentUserAuthorizationsHTMLURL {
+				t.Errorf("GetEndpoint() got = %v, want %v", got, tt.want)
+			}
+			if got.RepositoryURL != tt.want.RepositoryURL {
+				t.Errorf("GetEndpoint() got = %v, want %v", got, tt.want)
+			}
+			if got.CodeSearchURL != tt.want.CodeSearchURL {
+				t.Errorf("GetEndpoint() got = %v, want %v", got, tt.want)
+			}
+			if got.CommitSearchURL != tt.want.CommitSearchURL {
+				t.Errorf("GetEndpoint() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
