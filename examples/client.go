d22c724ab0b86db984e51228fdc9959017523f6e
Christos Ploutarchou <cploutarchou@gmail.com>
2022-12-26 01:11:10 +0200

    Update client config

--- examples/client.go
+++ examples/client.go
@@ -7,12 +7,17 @@ import (
 )
 
 var (
-	client = getClient()
-	baseURL = "https://go-requests.mocklab.io/"
+	client  = getClient()
+	baseURL = "https://63a8d253f4962215b588da7e.mockapi.io/api/v1/users/"
 )
 
 func getClient() http.Client {
 	builder := http.NewBuilder()
+	// set content type and accept to application/json
+	builder.Headers().
+		SetContentType("application/json").
+		SetAccept("application/json")
+
 	builder.SetRequestTimeout(50 * time.Second).
 		SetResponseTimeout(50 * time.Second).
 		SetMaxIdleConnections(10)
