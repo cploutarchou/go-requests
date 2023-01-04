d5eac37a664cd2b53b16547590d48478d47bf772
Christos Ploutarchou <cploutarchou@gmail.com>
2023-01-04 22:34:43 +0200

    fix make post requesst

--- http/client_test.go
+++ http/client_test.go
@@ -2,12 +2,13 @@ package http
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"reflect"
 	"testing"
 	"time"
 )
+
+var baseURL = "https://go-requests.wiremockapi.cloud"
 
 func Test_goHTTPClient_Get(t *testing.T) {
 	type args struct {
@@ -35,9 +36,10 @@ func Test_goHTTPClient_Get(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "Test_findPetsByTagXML",
+			name: "Test Get",
 			args: args{
-				tag: "cm8rvd96sgb7ev7dmli6pqz8vlpfx86egsiw6cejq1q1npe9yu45q27260b5td9ee90eiie7q49rb2xtmo26qq4shqfh6farkm8fz5ddpn7jq64dtdd16e1j8z99cesaxz65bj252y930hbsbfchir4l030z2rhuaf",
+				tag: "cm8rvd96sgb7ev7dmli6pqz8vlpfx86egsiw6cejq1q1npe9yu45q27260b5td9ee90eiie7" +
+					"q49rb2xtmo26qq4shqfh6farkm8fz5ddpn7jq64dtdd16e1j8z99cesaxz65bj252y930hbsbfchir4l030z2rhuaf",
 			},
 			want:    5,
 			wantErr: false,
@@ -55,14 +57,13 @@ func Test_goHTTPClient_Get(t *testing.T) {
 				SetMaxIdleConnections(10)
 			client := builder.Build()
 			client.QueryParams().Set("tags", tt.args.tag)
-			resp, err := client.Get("https://go-requests.wiremockapi.cloud/pet/findByTags", nil)
+			resp, err := client.Get(baseURL+"/pet/findByTags", nil)
 			if err != nil {
 				t.Errorf("findByTag() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			var pets PetsTags
-			err = resp.Unmarshal(&pets)
-			got := pets
+			var got PetsTags
+			err = resp.Unmarshal(&got)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("findByTag() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -76,70 +77,86 @@ func Test_goHTTPClient_Get(t *testing.T) {
 }
 
 func Test_goHTTPClient_Post(t *testing.T) {
-	type User struct {
-		Name     string `json:"name"`
-		UserName string `json:"username"`
-	}
-	user := User{
-		Name:     "Christos Ploutarchou",
-		UserName: "christos",
-	}
-	type fields struct {
-		Headers Headers
-		Timeout Timeout
+	type Order struct {
+		Id          int64     `json:"id"`
+		PetId       int64     `json:"petId"`
+		Quantity    int64     `json:"quantity"`
+		ShipDate    time.Time `json:"shipDate"`
+		Status      string    `json:"status"`
+		Complete    bool      `json:"complete"`
+		OrderStatus string    `json:"orderStatus"`
 	}
 	type args struct {
-		url string
+		item Order
 	}
+	t1, _ := time.Parse("2006-01-02 15:04:05", "2022-12-12 00:00:00")
 	tests := []struct {
 		name    string
-		fields  fields
 		args    args
-		want    *Response
+		want    Order
 		wantErr bool
 	}{
 		{
-			name: "Test goHTTPClient Post",
-			fields: fields{
-				Headers: NewHeaders(),
-				Timeout: newTimeouts(),
-			},
+			name: "Test Post",
 			args: args{
-				url: "https://api.github.com",
+				item: Order{
+					PetId:    670792158758028421,
+					Quantity: 2,
+					Id:       6075746898333402660,
+					ShipDate: t1,
+					Complete: true,
+					Status:   "approved",
+				},
 			},
-			want: &Response{
-				statusCode: 200,
-				status:     "200 OK",
+			want: Order{
+				PetId:    670792158758028421,
+				Quantity: 2,
+				Id:       6075746898333402660,
+				ShipDate: t1,
+				Complete: true,
+				Status:   "approved",
 			},
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
-		t.Run(
-			tt.name, func(t *testing.T) {
-				builder := NewBuilder()
-				client := builder.Build()
-				client.DisableTimeouts()
-				data, err := json.Marshal(user)
-				if err != nil {
-					t.Errorf("goHTTPClient.Post() Unable to marshal data.  error = %v, wantErr %v", err, tt.wantErr)
-					return
-				}
-				res, err := client.Post(tt.args.url, tt.fields.Headers.GetAll(), data)
-				got := &Response{
-					statusCode: res.StatusCode(),
-					status:     res.Status(),
-				}
-				fmt.Println(res.String())
-				if (err != nil) != tt.wantErr {
-					t.Errorf("goHTTPClient.Post() error = %v, wantErr %v", err, tt.wantErr)
-					return
-				}
-				if !reflect.DeepEqual(got, tt.want) {
-					t.Errorf("goHTTPClient.Post() =  %v, want %v", got, tt.want)
-				}
-			},
-		)
+		t.Run(tt.name, func(t *testing.T) {
+			data, err := json.Marshal(tt.args.item)
+			if err != nil {
+				t.Errorf("placeOrder() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			builder := NewBuilder()
+			builder.Headers().
+				SetContentType("application/json").
+				SetAccept("application/json")
+			
+			builder.SetRequestTimeout(10 * time.Second).
+				SetResponseTimeout(10 * time.Second).
+				SetMaxIdleConnections(10)
+			client := builder.Build()
+			res, err := client.Post(baseURL+"/store/order", nil, data)
+			if err != nil {
+				t.Errorf("placeOrder() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			var got Order
+			err = res.Unmarshal(&got)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("placePetOrder() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got.Id != tt.want.Id {
+				t.Errorf("placePetOrder() = %v, want %v", got.Id, tt.want.Id)
+				return
+			}
+			
+			// check if got is deep equal to want
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("placePetOrder() = \n%v \n, want \n%v \n", got, tt.want)
+				return
+			}
+		})
 	}
 }
 
